6631b2557af7fb78b9512e0f895d6302fe5f31ad
Daniel Osborne <github@danielosborne.net>
2015-05-16 01:57:50 -0700

    Add modding support for toggling the line-of-sight requirement for the laser antenna.

--- Sources/Sandbox.Common/ObjectBuilders/Definitions/MyObjectBuilder_LaserAntennaDefinition.cs
+++ Sources/Sandbox.Common/ObjectBuilders/Definitions/MyObjectBuilder_LaserAntennaDefinition.cs
@@ -21,5 +21,7 @@ namespace Sandbox.Common.ObjectBuilders.Definitions
         public float RotationRate = MathHelper.Pi / 20000.0f;
         [ProtoMember(5)]
         public float MaxRange = 40000;
+        [ProtoMember(6)]
+        public bool RequireLineOfSight = true;
     }
 }
