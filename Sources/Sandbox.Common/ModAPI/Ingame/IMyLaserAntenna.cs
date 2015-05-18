5f4bdcdb3f638420ba56bb4f68acb2c9ed292ec7
MeusRex <cedisto@gmx.ch>
2015-05-18 18:17:44 +0200

    Revert "No longer changes argument line."
    This reverts commit dca64c3f9c53cfec03902b33e866af572d43c02e.

--- Sources/Sandbox.Common/ModAPI/Ingame/IMyLaserAntenna.cs
+++ Sources/Sandbox.Common/ModAPI/Ingame/IMyLaserAntenna.cs
@@ -18,11 +18,20 @@ namespace Sandbox.ModAPI.Ingame
         void Connect();
 
         /// <summary>
-        /// Runs the script of a specified programmable block connected to the current grid via laser antenna with a custom argument.
+        /// Sets the argument for a specified programmable block connected to the current grid via laser antenna.
         /// </summary>
         /// <param name="pb_name">The name of the target programmable block for this message.</param>
-        /// <param name="message">The custom argument string.</param>
+        /// <param name="message">The new argument string.</param>
+        /// <param name="run">Should the programmable block be run after the argument was changed.</param>
         /// <returns>Returns true if the pb could be reached. False otherwise.</returns>
-        bool SendMessage(string pb_name, string message);
+        bool SendMessage(string pb_name, string message, bool run = false);
+
+        /// <summary>
+        /// Runs the script of a specified programmable block connected to the current grid via laser antenna.
+        /// </summary>
+        /// <param name="pb_name">The name of the target programmable block for this run command.</param>
+        /// <returns>Returns true if the pb could be reached. False otherwise.</returns>
+        bool SendRunCommand(string pb_name);
+
     }
 }
