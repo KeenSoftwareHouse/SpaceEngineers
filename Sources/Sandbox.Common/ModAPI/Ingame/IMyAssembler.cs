56d10faca65cd779f27bacb8cbdb35bdfb3b0ebf
Steven <steven@entityzero.com>
2015-05-20 02:34:53 +1200

    #96 Added Ingame Assembler Functions
    Added Ingame Assembler functions QueueItem and ClearQueue to enable
    players to be able to queue items in the assembler and clear the queue.
    QueueItem has three parameters, string itemType (I.e.
    MyObjectBuilder_Component), string subtypeName(I.e. SteelPlate), int
    amount
    QueueItem returns bool: true if item was queued, false if not. False
    indicates invalid type or subtype name.
    
    ClearItem has no parameters
    
    Both functions work in either assemble mode or disassemble mode.

--- Sources/Sandbox.Common/ModAPI/Ingame/IMyAssembler.cs
+++ Sources/Sandbox.Common/ModAPI/Ingame/IMyAssembler.cs
@@ -8,5 +8,7 @@ namespace Sandbox.ModAPI.Ingame
     public interface IMyAssembler : IMyProductionBlock
     {
         bool DisassembleEnabled { get; }
+        bool QueueItem(string itemType, string subtypeName, int amount);
+        void ClearQueue();
     }
 }
