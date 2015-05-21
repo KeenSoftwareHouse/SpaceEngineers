2b3de8d089afc3bb83d3b0bd551ecfa995e6637f
Josef Micka <josef.micka@gmail.com>
2015-05-21 11:19:39 +0200

    access fields for modified values on reffinery (eff,peff,prod)

--- Sources/Sandbox.Common/ModAPI/Ingame/IMyRefinery.cs
+++ Sources/Sandbox.Common/ModAPI/Ingame/IMyRefinery.cs
@@ -5,7 +5,22 @@ using System.Text;
 
 namespace Sandbox.ModAPI.Ingame
 {
+    /// <summary>
+    /// Refinery block interface
+    /// </summary>
     public interface IMyRefinery : IMyProductionBlock
     {
+        /// <summary>
+        /// Ore conversion Effectiveness - in decimals (0.5=50%)
+        /// </summary>
+        float Effectiveness { get; }
+        /// <summary>
+        /// Production speed - in decimals (0.5=50%)
+        /// </summary>
+        float Productivity { get; }
+        /// <summary>
+        /// Power efficiency - in decimals (0.5=50%)
+        /// </summary>
+        float PowerEfficiency { get; }
     }
 }
