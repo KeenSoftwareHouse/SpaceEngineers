db344271132fb8d1ac84e3b716b2ab6ed43723b8
Josef Micka <josef.micka@gmail.com>
2015-05-21 11:23:29 +0200

    upgrade fields for assemblers (peff, prod)

--- Sources/Sandbox.Common/ModAPI/Ingame/IMyAssembler.cs
+++ Sources/Sandbox.Common/ModAPI/Ingame/IMyAssembler.cs
@@ -5,8 +5,23 @@ using System.Text;
 
 namespace Sandbox.ModAPI.Ingame
 {
+    /// <summary>
+    /// Assembler block interface
+    /// </summary>
     public interface IMyAssembler : IMyProductionBlock
     {
+        /// <summary>
+        /// true - assembler in dissasemble mode, false - aseembler in normal mode
+        /// </summary>
         bool DisassembleEnabled { get; }
+
+        /// <summary>
+        /// Production speed - in decimals (0.5=50%)
+        /// </summary>
+        float Productivity { get; }
+        /// <summary>
+        /// Power efficiency - in decimals (0.5=50%)
+        /// </summary>
+        float PowerEfficiency { get; }
     }
 }
