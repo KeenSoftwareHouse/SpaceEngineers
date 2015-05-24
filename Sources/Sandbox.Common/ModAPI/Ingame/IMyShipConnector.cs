dbdb1ec78337d79d65a6ba39ee015633c27da8ea
Steven <steven@entityzero.com>
2015-05-24 16:59:07 +1200

    Missed comments from last commit
    nt

--- Sources/Sandbox.Common/ModAPI/Ingame/IMyShipConnector.cs
+++ Sources/Sandbox.Common/ModAPI/Ingame/IMyShipConnector.cs
@@ -7,10 +7,34 @@ namespace Sandbox.ModAPI.Ingame
 {
     public interface IMyShipConnector:IMyFunctionalBlock
     {
+        /// <summary>
+        /// Connector is set to throw out (Read Only)
+        /// </summary>
         bool ThrowOut { get; }
+
+        /// <summary>
+        /// Connector is set to pull items from connected inventories (Read Only)
+        /// </summary>
         bool CollectAll { get; }
+
+        /// <summary>
+        /// Connector is working and in range of another connector, could be connected or ready to lock (Read Only)
+        /// </summary>
         bool IsLocked { get; }
+
+        /// <summary>
+        /// Connector is connected to another connector (Read Only)
+        /// </summary>
         bool IsConnected { get; }
+
+        /// <summary>
+        /// Connector is in range and ready to lock with another connector, returns false if already connected. (Read Only)
+        /// </summary>
+        bool IsReadyToLock { get; }
+
+        /// <summary>
+        /// The other connector that this connector is connected too.
+        /// </summary>
         IMyShipConnector OtherConnector { get; }
     }
 }
