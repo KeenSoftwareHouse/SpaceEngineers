80d25338dd6b595c62ac802f15109abb11474798
Steven <steven@entityzero.com>
2015-05-25 05:17:51 +1200

    Minor comment change 3
    Whoops, Status instead of ConnectStatus

--- Sources/Sandbox.Common/ModAPI/Ingame/IMyShipConnector.cs
+++ Sources/Sandbox.Common/ModAPI/Ingame/IMyShipConnector.cs
@@ -18,17 +18,17 @@ namespace Sandbox.ModAPI.Ingame
         bool CollectAll { get; }
 
         /// <summary>
-        /// DEPRECATED use ConnectStatus
+        /// DEPRECATED use Status
         /// Connector is working and in range of another connector, could be connected or ready to lock (Read Only)
         /// </summary>
-        [Obsolete("Deprecated, Use ConnectStatus instead")]
+        [Obsolete("Deprecated, Use Status instead")]
         bool IsLocked { get; }
 
         /// <summary>
-        /// DEPRECATED use ConnectStatus
+        /// DEPRECATED use Status
         /// Connector is connected to another connector (Read Only)
         /// </summary>
-        [Obsolete("Deprecated, Use ConnectStatus instead")]
+        [Obsolete("Deprecated, Use Status instead")]
         bool IsConnected { get; }
 
         /// <summary>
