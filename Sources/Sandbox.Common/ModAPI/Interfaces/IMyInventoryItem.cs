870aa3f632632bdc5d5bdd66feebf87e6fcc9375
Steven <steven@entityzero.com>
2015-05-26 00:46:51 +1200

    IMyInventoryItem: Added OxygenBottleLevel
    Readyonly property OxygenBottleLevel for retrieving how full an oxygen
    bottle is.
    Returns -1 if called on a non OxygenContainerObject.
    Compiled and tested Ok.

--- Sources/Sandbox.Common/ModAPI/Interfaces/IMyInventoryItem.cs
+++ Sources/Sandbox.Common/ModAPI/Interfaces/IMyInventoryItem.cs
@@ -25,5 +25,12 @@ namespace Sandbox.ModAPI.Interfaces
             set;
         }
 
+        /// <summary>
+        /// Gets the level of an oxygen bottle
+        /// 0-1 representing % full, i.e. 0.5 = 50%
+        /// -1 if not an oxygen bottle
+        /// </summary>
+        float OxygenBottleLevel { get; }
+
     }
 }
