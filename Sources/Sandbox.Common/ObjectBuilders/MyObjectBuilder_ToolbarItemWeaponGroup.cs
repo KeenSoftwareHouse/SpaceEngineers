75550a1d7bcdeee09a4de5a57276ba1748f883c3
yajiedesign@gmail.com <yajiedesign@gmail.com>
2015-05-31 11:03:41 +0800

    add missing file

---
+++ Sources/Sandbox.Common/ObjectBuilders/MyObjectBuilder_ToolbarItemWeaponGroup.cs
@@ -0,0 +1,20 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox.Common.ObjectBuilders
+{
+    [ProtoContract]
+    [MyObjectBuilderDefinition]
+    public class MyObjectBuilder_ToolbarItemWeaponGroup : MyObjectBuilder_ToolbarItem
+    {
+        [ProtoMember]
+        public string GroupName { get; set; }
+        [ProtoMember]
+        public string DisplayNameText { get; set; }
+        [ProtoMember]
+        public string Icon { get; set; }
+    }
+}
