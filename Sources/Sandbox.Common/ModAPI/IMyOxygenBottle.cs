d3e0b3911524fd112bcb734e19379dcc4ddabf74
Josef Micka <josef.micka@gmail.com>
2015-06-20 13:40:08 +0200

    Interface to retrieve OxygenLevel and Capacity of oxygen container in inventory

---
+++ Sources/Sandbox.Common/ModAPI/IMyOxygenBottle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox.ModAPI
+{
+    /// <summary>
+    /// Interface for accessing oxygen bottle properties
+    /// </summary>
+    public interface IMyOxygenBottle : Ingame.IMyOxygenBottle
+    {
+        /* SET is not safe atm. not implementing
+        /// <summary>
+        /// Current oxygen level
+        /// </summary>
+        float OxygenLevel { get; set; }
+        */
+    }
+}
