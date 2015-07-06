fcb522186b346eaba2e35ab3a8d91bbe9b65623a
Bradley Regula <bluegm@gmail.com>
2015-07-06 01:13:54 -0400

    Fixed error with the way piston limits were saved.
    To reproduce the error, assign the ExtendedPistonBase object in cubeblocks.sbc a property <Minimum>5.0</Minimum>. Then, in the game, set the minimum and/or maximum limit of a piston, save the game and reload. The limits restored will not be the same as what was saved.

--- Sources/Sandbox.Game/Game/Entities/Blocks/MyPistonBase.cs
+++ Sources/Sandbox.Game/Game/Entities/Blocks/MyPistonBase.cs
@@ -255,7 +255,7 @@ namespace Sandbox.Game.Entities.Blocks
 
         private float NormalizeDistance(float value)
         {
-            return (value + BlockDefinition.Minimum) / Range;
+            return (value - BlockDefinition.Minimum) / Range;
         }
 
         private float DenormalizeDistance(float value)
