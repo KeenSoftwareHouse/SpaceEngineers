51d942b3b7001f07a73c87333d498f2b1ffc373b
leftler <leftler@gmail.com>
2015-09-27 16:24:59 -0500

    - Changed the interfaces to follow KSWH standards of Readonly properties, methods for writers. - Changed the wording of some comments

--- Sources/Sandbox.Game/Game/Entities/Blocks/MyJumpDrive.cs
+++ Sources/Sandbox.Game/Game/Entities/Blocks/MyJumpDrive.cs
@@ -836,11 +836,12 @@ namespace Sandbox.Game.Entities
         public float JumpDistanceRatio
         {
             get { return m_jumpDistanceRatio; }
-            set
-            {
-                var clampedValue = VRageMath.MathHelper.Clamp(value, 0f, 100f);
-                SetJumpDistanceRatio(clampedValue);
-            }
+        }
+
+        public void RequestJumpDistanceRatio(float ratio)
+        {
+            var clampedValue = VRageMath.MathHelper.Clamp(ratio, 0f, 100f);
+            SetJumpDistanceRatio(clampedValue);
         }
 
         public float PowerNeededForJump
@@ -861,7 +862,11 @@ namespace Sandbox.Game.Entities
         public bool Recharging
         {
             get { return m_isRecharging; }
-            set { SetRecharging(value); }
+        }
+
+        public void RequestRecharging(bool enabled)
+        {
+            SetRecharging(enabled);
         }
 
         public void SetTarget(Vector3D cords, string name)
