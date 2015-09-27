97a68c9153768276296c30b477c151dbc1b17f21
leftler <leftler@gmail.com>
2015-09-27 15:37:02 -0500

    Added 3 more parameters of info that is normally displayed on the GUI. Also added comments to all of the members in the interface.

--- Sources/Sandbox.Common/ModAPI/Ingame/IMyJumpDrive.cs
+++ Sources/Sandbox.Common/ModAPI/Ingame/IMyJumpDrive.cs
@@ -5,8 +5,14 @@ namespace Sandbox.ModAPI.Ingame
 {
     public interface IMyJumpDrive : IMyFunctionalBlock
     {
+        /// <summary>
+        /// Is the jumpdrive in a state that would allow a jump.
+        /// </summary>
         bool CanJump { get; }
 
+        /// <summary>
+        /// Is the jumpdrive full of energy.
+        /// </summary>
         bool IsFull { get; }
 
         /// <summary>
@@ -14,18 +20,42 @@ namespace Sandbox.ModAPI.Ingame
         /// </summary>
         float JumpDistanceRatio { get; set; }
 
+        /// <summary>
+        /// The amount of power tht needs to be stored to perform a jump in Megawats.
+        /// </summary>
+        float PowerNeededForJump { get; }
+         
+        /// <summary>
+        /// The amount of power stored in the jumpdrive in Megawats.
+        /// </summary>
+        float StoredPower { get; }
+
+        /// <summary>
+        /// The number of seconds remaining in the recharge time.
+        /// </summary>
+        float RechargeTimeRemaining { get; }
+
+        /// <summary>
+        /// Should the jump drive draw power to recharge itself.
+        /// </summary>
         bool Recharging { get; set; }
 
 
-
+        /// <summary>
+        /// Returns the maximum distance in meeters that the jumpdrive will jump with the current settings.
+        /// </summary>
         double ComputeMaxDistance();
 
+        /// <summary>
+        /// Returns if the drive is ready to jump and could be jumped by the specified user.
+        /// </summary>
+        /// <param name="userId">The userId to check against.</param>
         bool CanJumpAndHasAccess(long userId);
 
         void RemoveSelected();
 
         /// <summary>
-        /// Manually sets the target jump point
+        /// Manually sets the target jump point.
         /// </summary>
         /// <param name="cords">The cordinates to jump to.</param>
         /// <param name="name">The name of the GPS point to display in the GUI.</param>
