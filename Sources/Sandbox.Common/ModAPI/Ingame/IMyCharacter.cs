b871c37e58285a5b6ff5ede3c7be5df7dd985d6f
plaYer2k <nitschke.d@gmail.com>
2016-03-16 12:56:25 +0100

    Added Ingame.IMyCharacter with exposed long PlayerId and MatrixD HeadMatrix

---
+++ Sources/Sandbox.Common/ModAPI/Ingame/IMyCharacter.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox.Common.ObjectBuilders;
+using Sandbox.ModAPI.Interfaces;
+using VRage.Game;
+
+namespace Sandbox.ModAPI.Ingame
+{
+    public interface IMyCharacter
+    {
+        VRageMath.MatrixD HeadMatrix { get; }
+        long PlayerId { get; }
+    }
+}
