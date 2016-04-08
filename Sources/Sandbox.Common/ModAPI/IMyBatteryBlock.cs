c94c5a0894b90ea46d3bfcb6bf3bb3720cef7c2c
yajiedesign <yajiedesign@gmail.com>
2016-04-08 10:00:30 +0800

    add CurrentStoredPower set

---
+++ Sources/Sandbox.Common/ModAPI/IMyBatteryBlock.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox.ModAPI
+{
+    public interface IMyBatteryBlock : Ingame.IMyBatteryBlock
+    {
+        new float CurrentStoredPower { get; set; }
+  
+    }
+}
