155b4310ecdf61af5de834196459fc120a86022e
sebeksd <sebeksd@users.noreply.github.com>
2016-05-01 13:36:42 +0200

    Corrected Pack64() calculation for negative x/y/z values and removed warning (CS0675), this commit should be reviewed

--- Sources/Sandbox.Game/Game/Entities/Planet/MyPlanetEnvironmentSector.cs
+++ Sources/Sandbox.Game/Game/Entities/Planet/MyPlanetEnvironmentSector.cs
@@ -111,9 +111,9 @@ namespace Sandbox.Game.Entities
 
         public long Pack64()
         {
-            long value = Position.X
-                         | ((long)Position.Y << 16)
-                         | ((long)Position.Z << 32);
+            long value = ((long)Position.X & 0xFFFF)
+                         | (((long)Position.Y & 0xFFFF) << 16)
+                         | (((long)Position.Z & 0xFFFF) << 32);
 
             Vector3I dirv = Direction;
             var dir = (long)Base6Directions.GetDirection(ref dirv);
