7b76a15d3a00fa463c289e5b1d43f76e70fe132f
haveachillday <haveachillday@gmail.com>
2016-10-12 13:39:12 +1100

    Revert "finished the pull request"
    This reverts commit 246350568cc1b3982e81f6ef40f813c278e3be76.

--- Sources/Sandbox.Common/ModAPI/Ingame/IMyOreDetector.cs
+++ Sources/Sandbox.Common/ModAPI/Ingame/IMyOreDetector.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using VRageMath;
 
 namespace Sandbox.ModAPI.Ingame
 {
@@ -10,23 +9,5 @@ namespace Sandbox.ModAPI.Ingame
     {
         float Range {get;}
         bool BroadcastUsingAntennas {get;}
-
-        /// <summary>
-        ///Returns a list of ore deposits.
-        ///Each ore deposit has a substance name and a location.
-        /// </summary>        
-        List <OreDeposit> GetOreMarkers();        
-    }
-
-    public struct OreDeposit
-    {
-        public readonly string substanceName;
-        public readonly Vector3D coordinates;
-
-        public OreDeposit (string inputName, Vector3D inputCoords)
-        {
-            this.substanceName = inputName;
-            this.coordinates = inputCoords;
-        }
     }
 }
--- Sources/Sandbox.Game/Game/Entities/Blocks/MyOreDetector.cs
+++ Sources/Sandbox.Game/Game/Entities/Blocks/MyOreDetector.cs
@@ -1,7 +1,5 @@
 ﻿#region Using
 
-using System.Collections.Generic;
-
 using Sandbox.Common.ObjectBuilders;
 using Sandbox.Definitions;
 using Sandbox.Game.Gui;
@@ -205,22 +203,5 @@ namespace Sandbox.Game.Entities.Cube
 
         bool ModAPI.Ingame.IMyOreDetector.BroadcastUsingAntennas { get { return m_oreDetectorComponent.BroadcastUsingAntennas; } }
         float ModAPI.Ingame.IMyOreDetector.Range { get { return Range; } }
-
-        public List <ModAPI.Ingame.OreDeposit> GetOreMarkers() 
-        {
-            List <ModAPI.Ingame.OreDeposit> oreMarkers = new List <ModAPI.Ingame.OreDeposit>();
-            
-            foreach (MyEntityOreDeposit deposit in MyHud.OreMarkers)
-            {
-                List <MyEntityOreDeposit.Data> markers = deposit.Materials;
-
-                for (int i = 0; i < markers.Count; i++)
-                {
-                    string substanceName = markers[i].Material.MaterialTypeName;
-                    oreMarkers.Add (new ModAPI.Ingame.OreDeposit (substanceName, markers[i].AverageLocalPosition));
-                }
-            }
-            return oreMarkers;
-        }
     }
 }
