2a6f5111e0fdf2ff3ac6fd5a0700c54268e36ba9
Roof Level Suite <haveachillday@gmail.com>
2016-10-12 12:17:52 +1100

    refined a couple names.

--- Sources/Sandbox.Common/ModAPI/Ingame/IMyOreDetector.cs
+++ Sources/Sandbox.Common/ModAPI/Ingame/IMyOreDetector.cs
@@ -13,19 +13,19 @@ namespace Sandbox.ModAPI.Ingame
 
         /// <summary>
         ///Returns a list of ore deposits.
-        ///Each ore deposit has a substance name and a location.
+        ///Each ore deposit has an element name and a location.
         /// </summary>        
         List <OreDeposit> GetOreMarkers();        
     }
 
     public struct OreDeposit
     {
-        public readonly string substanceName;
+        public readonly string elementName;
         public readonly Vector3D coordinates;
 
         public OreDeposit (string inputName, Vector3D inputCoords)
         {
-            this.substanceName = inputName;
+            this.elementName = inputName;
             this.coordinates = inputCoords;
         }
     }
