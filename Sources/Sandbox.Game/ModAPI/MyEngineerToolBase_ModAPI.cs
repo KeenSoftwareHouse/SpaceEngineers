523a595ac79cef76246d7c73dd638890e8c8bd24
Dummy0815 <thomas_stamm@arcor.de>
2017-01-04 01:01:52 +0100

    Add files via upload

---
+++ Sources/Sandbox.Game/ModAPI/MyEngineerToolBase_ModAPI.cs
@@ -0,0 +1,16 @@
+﻿using VRage.Game.ModAPI;
+using Sandbox.ModAPI.Weapons;
+
+namespace Sandbox.Game.Weapons
+{
+    public abstract partial class MyEngineerToolBase : IMyEngineerToolBase
+    {
+      IMyCharacter IMyEngineerToolBase.Owner
+      {
+         get
+         {
+            return Owner;
+         }
+      }
+   }
+}
